c2de5c9f4dff3a0ccb98746a63f33311af68fc76
DDR1705 <44269913+DDR1705@users.noreply.github.com>
2018-11-22 12:28:23 +0100

    Rozdizał 2 nagranie 011 "Action Parameters".

---
+++ Platforma/Platforma/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Platforma.Models; // Żeby mieć moliwość wywoływania z klas modelu
+
+namespace Platforma.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Terminator" }; // Użycie pola Name z Models/Movie dzięki using Platforma.Models
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+
+        }
+        public ActionResult Index (int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex = {0} &sortBy= {1}", pageIndex, sortBy));
+
+        }
+    }
+}
---
+++ Platforma/Platforma/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Platforma.MvcApplication" Language="C#" %>
