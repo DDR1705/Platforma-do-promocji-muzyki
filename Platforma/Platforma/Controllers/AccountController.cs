50114e9fa5e782cfb729124dbda6854220927a7f
DDR1705 <44269913+DDR1705@users.noreply.github.com>
2018-11-29 13:38:53 +0100

    We wcześniejszym projekcie zapomniałem o logowaniu i rejestracji. Po próbach dodania tych funkcji do istniejącego projektu napotkałem błędy, których nie byłem w stanie naprawić . Obecnie nowy projekt zaczynam od miejsca, w którym skończyłem poprzednią wersję.

--- Platforma/Platforma/Controllers/AccountController.cs
+++ Platforma/Platforma/Controllers/AccountController.cs
@@ -13,5 +13,24 @@ namespace Platforma.Controllers
         {
             return View();
         }
+
+        internal class ChallengeResult : ActionResult
+        {
+            private string provider;
+            private string v1;
+            private string v2;
+
+            public ChallengeResult(string provider, string v1, string v2)
+            {
+                this.provider = provider;
+                this.v1 = v1;
+                this.v2 = v2;
+            }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
---
+++ VideoStore/VideoStore/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace VideoStore
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes(); // Aktywacja mechanizmu routingu na bazie atrybutów. Od teraz reguły można umieszczać bezpośrednio w kontrolerach.
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
