73b83c7d8031c1e90a5685b0a479032e812fbf79
DDR1705 <44269913+DDR1705@users.noreply.github.com>
2018-11-30 11:19:22 +0100

    Nagranie 26 Zmiany w modelu

---
+++ VideoStore/VideoStore/Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoStore.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; } //membership type
+        public short SingUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+        public byte MembershipTypeId { get; set; }
+    }
+}
