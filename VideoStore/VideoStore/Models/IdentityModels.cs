83dc4135895f3fe34b6589a5e01e350a8181154b
DDR1705 <44269913+DDR1705@users.noreply.github.com>
2018-11-30 10:55:37 +0100

    Rozdział 3  nagranie  25.  Baza danych - code first

--- VideoStore/VideoStore/Models/IdentityModels.cs
+++ VideoStore/VideoStore/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace VideoStore.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
