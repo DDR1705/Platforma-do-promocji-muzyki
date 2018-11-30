860e5106645da39937f923f30b70f8fca09312fb
DDR1705 <44269913+DDR1705@users.noreply.github.com>
2018-11-30 14:57:25 +0100

    Nagranie 27 Overriding Conventions

--- VideoStore/VideoStore/Models/Customer.cs
+++ VideoStore/VideoStore/Models/Customer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations; // [Required]
 
 namespace VideoStore.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(256)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public  MembershipType MembershipType{ get; set; }
